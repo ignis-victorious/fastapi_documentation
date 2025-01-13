810490a439a806584ab7dc1bde2a9a6d97ba3810
Emagnu <ignisvictorious@gmail.com>
2025-01-13 14:46:42 +0000

    Order of Path Parameter

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+#  Define path parameters must precede general/variable path parameter
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
 #  Path Parameter "int" also function complete an operation and return new value
 @app.get("/items/{item_id}")
-async def read_item(item_id: int) -> dict[str, int]:
-    return {"item_id": item_id * 50}
+async def read_item(item_id: str) -> dict[str, str]:
+    return {"item_id": item_id}
 
 
 #  Path Parameter "str"
